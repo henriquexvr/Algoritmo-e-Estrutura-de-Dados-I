cb7d7343539b4e16d19e871d2216dd8c6204dc84
henriquexvr <henriquecaldeirax3@gmail.com>
2025-09-20 11:19:24 -0300

    Correção de um exercicio de modularização

--- exercicios/Modularizacao/FuncaoNotasAlunos.c
+++ exercicios/Modularizacao/FuncaoNotasAlunos.c
@@ -1,16 +1,26 @@
+/*Fa¸ca uma fun¸c˜ao que lˆe um n´umero indeterminado de notas de alunos, calcula e retorna
+a m´edia das notas dos alunos aprovados (nota maior ou igual a 6). Fa¸ca um programa que lˆe o n´umero
+de alunos e imprime a m´edia retornada pela fun¸c˜ao.*/
 #include <stdio.h>
 
 float calculoMediaNotas(int numeroAlunos){
-    float notaAluno, mediaNotas = 0.0;
+    int contadorAlunos = numeroAlunos;
+    float notaAluno, mediaNotas, totalNotas = 0.0;
     for(int i = 0; i < numeroAlunos; i++){
-        printf("NOTA ALUNO %d", i);
-        scanf("%f", &notaAluno);
-        if((int)notaAluno > 6){
-            mediaNotas = (mediaNotas + notaAluno) / (float)numeroAlunos; 
-        }else{
-            numeroAlunos--;
-        }
+        do{
+            printf("NOTA ALUNO %d:\n", i+1);
+            scanf("%f", &notaAluno);
+            if(notaAluno >= 6.0 && ( notaAluno <= 10.0)){
+                totalNotas = totalNotas + notaAluno;
+            }else if(notaAluno < 6.0 && notaAluno >= 0.0){
+                contadorAlunos--;
+            }
+            else{
+                printf("Valor invalido! Escreva novamente\n");
+            }
+        }while(notaAluno < 0.0 || notaAluno > 10.0);
     }
+        mediaNotas = totalNotas / (float)contadorAlunos;
     return mediaNotas;
 }
 
@@ -20,7 +30,7 @@ int main(){
     
     printf("Escreva o numero de alunos:\n");
     scanf("%d", &numAlunos);
-    calculoMediaNotas(numAlunos);
-
-    printf("Media das notas = %.2f", calculoMediaNotas(mediaNotas));
+    mediaNotas = calculoMediaNotas(numAlunos);
+    
+    printf("MEDIA DAS NOTAS DOS ALUNOS APROVADOS = %.2f", mediaNotas);
 }
