b15797dabbb38bc47f88919f8fb057c33858bb82
henriquexvr <henriquecaldeirax3@gmail.com>
2025-10-19 12:11:01 -0300

    Atualizações do Sistema Missão Espacial

--- Missão Espacial/versao00.c
+++ Missão Espacial/versao00.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 int gravidadePlaneta(){
     int escolhaPlaneta;
     printf("Digite um numero inteiro para fazer a escolha:\n");
@@ -10,89 +9,111 @@ int gravidadePlaneta(){
     switch(escolhaPlaneta){
 
             case 1:
-            printf("Marte:\n Recomendado a alterar a gravidade para: 3.73m/s\n");
+            printf("Marte\n Recomendado a alterar a gravidade para: 3.73m/s\n");
             break;
             case 2:
-            printf("Saturno:\n Recomendado a alterar a gravidade para: 10.44m/s\n");
+            printf("Saturno\n Recomendado a alterar a gravidade para: 10.44m/s\n");
             break;
             case 3:
-            printf("Jupiter:\n Recomendado a alterar a gravidade para: 24.79m/s\n");
+            printf("Jupiter\n Recomendado a alterar a gravidade para: 24.79m/s\n");
             break;
         }
     return escolhaPlaneta;
     }
-
-float calcularAngulo(){
-    float anguloTrajetoria = 0;
+int perguntarDistancia(){
     int distancia;
     printf("Escreva a distancia que voce quer viajar:\n");
     scanf("%d", &distancia);
-
+    printf("A distancia eh: %d\n", distancia);
+    return distancia;
+}
+float calcularAngulo(){
+    float anguloTrajetoria = 0;
+    int distancia = perguntarDistancia();
+    
     anguloTrajetoria = distancia * 0.42 + 18;
 
     printf("O angulo de trajetorioa eh: %.2f\n", anguloTrajetoria);
-    return anguloTrajetoria;
+    return 0;
 }
 
-int simularQueima(){
-    int combustivel = 1000;
+int simularQueima(int combustivel){
     //Cada interação com o combustivel reduz 50 unidades
-    printf("NIVEL DE COMBUSTIVEL = 1000L\n");
-    combustivel = combustivel - 50;
-    if(combustivel == 500){
-        printf("O NIVEL DE COMBUSTIVEL CHEGOU A: 500L");
-    }else if(combustivel == 100){
-        printf("O NIVEL DE COMBUSTIVEL CHEGOU A: 100L ");
+    if(combustivel >= 50){
+            combustivel = combustivel - 50;
+        printf("%d\n", combustivel);
+        if(combustivel > 500){
+            printf("NIVEL DE COMBUSTIVEL = 1000L\n");
+        }
+        else if(combustivel == 500){
+            printf("O NIVEL DE COMBUSTIVEL CHEGOU A: 500L\n");
+        }else if(combustivel == 100){
+            printf("ATENÇÃO: O NIVEL DE COMBUSTIVEL CHEGOU A: 100L\n");
+        }else if(combustivel == 0){
+            printf("ATENÇÃO: O NIVEL DE COMBUSTIVEL CHEGOU A: 0L\n");
+        }
+    }else{
+        printf("AVISO: COMBUSTIVEL INSUFICIENTE PARA QUEIMA");
     }
-return combustivel;
+    
+    return combustivel;
+
 }
-
-float registrarMissao(){
-    float registroMissoes[40];
-    int numeroMissao = 0;
-    int indice = 0;
-
-    if (numeroMissao >= 10) {
+float registrarMissao(float registroMissoes[], int *numMissoes){
+    
+    if (*numMissoes >= 10) {
         printf("Limite de missoes atingido!\n");
         return;
     }
-    printf("Escreva  a distancia da missao: ");
+
+    int indice = (*numMissoes * 4);
+    
+    printf("========REGISTRAR MISSAO %d========\n", (*numMissoes) + 1);
+
+    printf("Escreva  a distancia:\n");
     scanf("%f", &registroMissoes[0 + indice]);
 
-    printf("Escreva  o angulo da trajetoria: ");
+    printf("Escreva  o angulo da trajetoria:\n");
     scanf("%f", &registroMissoes[1 + indice]);
-    
 
-    printf("Escreva o numero do planeta:\n1. Marte\n2. Jupiter\n3. Saturno");
+    printf("Escreva o numero do planeta:\n1. Marte\n2. Jupiter\n3. Saturno\n");
     scanf("%f", &registroMissoes[2 + indice]);
     
 
-    printf("Escreva o status final da missao:\n1 = Sucesso\n0 = Fracassou");
+    printf("Escreva o status final da missao:\n1 = Sucesso\n0 = Fracassou\n");
     scanf("%f", &registroMissoes[3 + indice]);
      
     
-    printf("Missão %d registrada com sucesso!", numeroMissao);
-    numeroMissao++;
-    indice = indice + 4;
-    
-    return registroMissoes, 40;
+    printf("Missao %d registrada com sucesso!\n", (*numMissoes)+1);
+    (*numMissoes)++;
+
+    return 0;
 }
 
-void historicoMissoes(){
-    float registroMissoes[40];
-    int numeroMissoes;
-    int multiplicador = 4, indice;
-    for(int i = 0; i <= numeroMissoes; i++){
-        indice = i * multiplicador;
-        printf("Missao %d:", i+ 1);
-        printf("Distancia da missão: %.2f", registroMissoes[indice]);
-        printf("Angulo trajetoria: %.2f", registroMissoes[indice + 1]);
-        printf("Planeta: %d", registroMissoes[indice + 2]);
-        printf("Status missão %d", registroMissoes[indice + 3]);
+float historicoMissoes(float registroMissoes[], int numeroMissoes){
+    printf("%d\n", numeroMissoes);
+    for(int i = 0; i < numeroMissoes; i++){
+        int indice = i * 4;
+        
+        int distancia = registroMissoes[indice];
+        int angulo = registroMissoes[indice + 1];
+        int planeta = registroMissoes[indice + 2];
+        int status = registroMissoes[indice + 3];
+
+        printf("====MISSAO %d:====\n", i+ 1);
+        printf("Distancia da missao: %d\n", distancia);
+        printf("Angulo trajetoria: %d\n", angulo);
+        printf("Planeta: %d\n", planeta);
+        printf("Status missao: %d\n", status);
     }
+return 0; 
 }
+
 int menu(){
     int escolhaOpcao;
+    //Variaveis globais
+    int combustivel = 1000, numeroDeMissoes = 0;
+    float registroMissoes[40];
     do{
         printf("==== SISTEMA DE CONTROLE DE MISSOES ESPACIAIS ====\n");
         printf("Digite um numero inteiro para fazer a escolha:\n");
@@ -101,7 +122,7 @@ int menu(){
         scanf("%d", &escolhaOpcao);
         switch(escolhaOpcao){
             case 1:
-            simularQueima();
+            combustivel = simularQueima(combustivel);
             break;
             case 2:
             calcularAngulo();
@@ -110,10 +131,10 @@ int menu(){
             gravidadePlaneta();
             break;
             case 4:
-            registrarMissao();
+            registrarMissao(registroMissoes, &numeroDeMissoes);
             break;
             case 5:
-            historicoMissoes();
+            historicoMissoes(registroMissoes, numeroDeMissoes);
             break;
             case 6:
             printf("Finalizando o sistema.\n");
@@ -129,6 +150,7 @@ int menu(){
 int main(){
     int senha = 2025, tentativas = 0;
     do{
+        tentativas++;
         printf("Escreva a senha:\n");
         scanf("%d", &senha);
         
@@ -136,7 +158,7 @@ int main(){
             printf("Numero de tentativas esgotado!");
             return 0;
         }
-        tentativas++;
+        
     }while(senha != 2025);
     menu();
     
